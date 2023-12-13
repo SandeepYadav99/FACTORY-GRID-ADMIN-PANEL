8056d4cdd2ebf0353db3ce9225ea63e274905cd7
sandeep.electrovese <sandeepelectrovese@gmail.com>
2023-12-13 16:41:53 +0530

    Reuse Component add for Customer View

--- src/views/Customers/components/Info/Business.view.js
+++ src/views/Customers/components/Info/Business.view.js
@@ -1,76 +1,51 @@
-import React, {Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {Button, ButtonBase, Paper, withStyles} from "@material-ui/core";
-import styles from './style.module.css'
+import React from "react";
 
-class BusinessView extends Component {
-    constructor(props) {
-        super(props);
+import { Paper } from "@material-ui/core";
+import styles from "./style.module.css";
 
-        this.state = {
-
-        }
-
-    }
-
-    render() {
-        const {data} = this.props;
-        return (
-            <Paper>
-            <div className={'midContainer'}>
-                <div className={'formFlex'}>
-                    <div className={'formGroup'}>
-                        <label>Company Name</label>
-                        <div>
-                            {/*<img className={styles.userImage} src={data.user_image}/>*/}
-                        </div>
-                    </div>
-
-                    <div className={'formGroup'}>
-                        <label>Company Logo</label>
-                        <div>
-                            {/*<img className={styles.userImage} src={data.user_image}/>*/}
-                        </div>
-                    </div>
-                </div>
-
-
+const BusinessView = () => {
+  return (
+    <Paper>
+      <div className={"midContainer"}>
+        <div className={"formFlex"}>
+          <div className={"formGroup"}>
+            <label>Company Name</label>
+            <div>
+              {/*<img className={styles.userImage} src={data.user_image}/>*/}
             </div>
-            </Paper>
-        )
-    }
-}
+          </div>
 
-const useStyle = theme => ({
-    btnSuccess: {
-        backgroundColor: theme.palette.success.dark,
-        color: 'white',
-        marginRight: 5,
-        marginLeft: 5,
-        '&:hover': {
-            backgroundColor: theme.palette.success.main,
-        }
+          <div className={"formGroup"}>
+            <label>Company Logo</label>
+            <div>
+              {/*<img className={styles.userImage} src={data.user_image}/>*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Paper>
+  );
+};
+
+const useStyle = (theme) => ({
+  btnSuccess: {
+    backgroundColor: theme.palette.success.dark,
+    color: "white",
+    marginRight: 5,
+    marginLeft: 5,
+    "&:hover": {
+      backgroundColor: theme.palette.success.main,
     },
-    btnError: {
-        backgroundColor: theme.palette.error.dark,
-        color: 'white',
-        marginLeft: 5,
-        marginRight: 5,
-        '&:hover': {
-            backgroundColor: theme.palette.error.main,
-        }
-    }
+  },
+  btnError: {
+    backgroundColor: theme.palette.error.dark,
+    color: "white",
+    marginLeft: 5,
+    marginRight: 5,
+    "&:hover": {
+      backgroundColor: theme.palette.error.main,
+    },
+  },
 });
 
-
-function mapStateToProps(state) {
-    return {};
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({}, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)((withStyles(useStyle, {withTheme: true})(BusinessView)));
+export default BusinessView;
