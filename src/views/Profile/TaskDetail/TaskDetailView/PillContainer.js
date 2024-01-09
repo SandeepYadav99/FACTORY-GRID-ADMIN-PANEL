f98e6a29d0d5fa8443fbc3de52e7f1d0c0239681
sandeep.electrovese <sandeepelectrovese@gmail.com>
2024-01-09 18:54:40 +0530

    Fix Pill Container bg colro

--- src/views/Profile/TaskDetail/TaskDetailView/PillContainer.js
+++ src/views/Profile/TaskDetail/TaskDetailView/PillContainer.js
@@ -1,11 +1,23 @@
 import React, { memo } from "react";
 
 const PillContainer = ({ details, styles }) => {
+  const getPriorityColor = (priority) => {
+    switch (priority) {
+      case 'HIGH':
+        return '#FF0000';
+      case 'MEDIUM':
+        return '#FA8B55';
+      case 'LOW':
+        return '#15F205';
+      default:
+        return '#FFFFFF'; 
+    }
+  };
   return (
     <div>
       {" "}
       <div className={styles.pillContainer}>
-        <div className={styles.priority}>{details?.priority}</div>
+        <div className={styles.priority} style={{ backgroundColor: getPriorityColor(details?.priority) }}>{details?.priority}</div>
         <div className={styles.section}>{details?.type}</div>
       </div>
       <div className={styles.gaps} />
