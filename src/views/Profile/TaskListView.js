3f184d13a45c70bc21c9394f84a22a053c85c845
sandeep.electrovese <sandeepelectrovese@gmail.com>
2024-01-09 18:53:02 +0530

    Fix Layout

--- src/views/Profile/TaskListView.js
+++ src/views/Profile/TaskListView.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import EmailIcon from '@material-ui/icons/Email';
-import styles from './Styles.module.css'; // Replace with the actual import for your styles
+import styles from './Styles.module.css'; 
 
 const TaskListItem = ({ task, handleDetailPage }) => {
+  const getPriorityColor = (priority) => {
+    switch (priority) {
+      case 'HIGH':
+        return '#FF0000';
+      case 'MEDIUM':
+        return '#FA8B55';
+      case 'LOW':
+        return '#15F205';
+      default:
+        return '#FFFFFF'; 
+    }
+  };
   return (
     <div onClick={() => handleDetailPage(task)} className={styles.detailView}>
       <div className={styles.check}>
@@ -17,7 +29,7 @@ const TaskListItem = ({ task, handleDetailPage }) => {
           <EmailIcon className={styles.contactIcons} />
           <span className={styles.info}>{task?.dueDateText}</span>
         </div>
-        <div className={styles.priority}>{task?.priority}</div>
+        <div className={styles.priority} style={{ backgroundColor: getPriorityColor(task?.priority) }}>{task?.priority}</div>
         <div className={styles.section}>{task?.type}</div>
       </div>
       <div className={styles.gaps} />
